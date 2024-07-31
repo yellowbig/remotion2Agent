59bb5e2fd2e3ed718019e8410abe8281cf6e4e9d
史方明 <dahwang@126.com>
2024-07-31 08:51:50 +0800

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,5 +3,3 @@
 # 安装必要的库
 apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
 
-# 运行原来的构建命令
-npm run build
