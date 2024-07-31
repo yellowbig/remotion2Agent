4bd39a2615a1092cb433c099bfb3ddbc43abe7ca
史方明 <dahwang@126.com>
2024-07-31 09:21:43 +0800

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# 安装必要的库
-apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
+# 安装项目依赖
+npm install
+
+# 执行 Remotion 渲染
+npx remotion render MyVideo out/video.mp4
 
